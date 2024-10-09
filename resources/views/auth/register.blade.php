b554c30f5e138ae91f7d6b762b8126ae8f1ced14
Faizal <91838778+FaizalLeviansyah@users.noreply.github.com>
2024-10-09 14:06:43 +0700

    perubahan HopeUI

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,80 +1,110 @@
-@extends('layouts.app')
+<x-guest-layout>
+   <section class="login-content">
+      <div class="row m-0 align-items-center bg-white vh-100">            
+         <div class="col-md-6 d-md-block d-none bg-primary p-0 mt-n1 vh-100 overflow-hidden">
+            <img src="{{asset('images/auth/05.png')}}" class="img-fluid gradient-main animated-scaleX" alt="images">
+         </div>
+         <div class="col-md-6">           
+            <div class="row justify-content-center">
+               <div class="col-md-10">
+                  <div class="card card-transparent auth-card shadow-none d-flex justify-content-center mb-0">
+                     <div class="card-body">
+                        <a href="{{route('dashboard')}}" class="navbar-brand d-flex align-items-center mb-3">
+                           <svg width="30" class="text-primary" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <rect x="-0.757324" y="19.2427" width="28" height="4" rx="2" transform="rotate(-45 -0.757324 19.2427)" fill="currentColor"/>
+                              <rect x="7.72803" y="27.728" width="28" height="4" rx="2" transform="rotate(-45 7.72803 27.728)" fill="currentColor"/>
+                              <rect x="10.5366" y="16.3945" width="16" height="4" rx="2" transform="rotate(45 10.5366 16.3945)" fill="currentColor"/>
+                              <rect x="10.5562" y="-0.556152" width="28" height="4" rx="2" transform="rotate(45 10.5562 -0.556152)" fill="currentColor"/>
+                           </svg>
+                           <h4 class="logo-title ms-3">{{env('APP_NAME')}}</h4>
+                        </a>
+                        <h2 class="mb-2 text-center">Register</h2>
+                        <p class="text-center">Create your {{env('APP_NAME')}} account.</p>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                        <!-- Form Registration -->
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                           <div class="row">
+                              <!-- Full Name -->
+                              <div class="col-lg-6">
+                                 <div class="form-group">
+                                    <label for="name" class="form-label">Name</label>
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autofocus>
+                                    @error('name')
+                                       <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                       </span>
+                                    @enderror
+                                 </div>
+                              </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                              <!-- Email -->
+                              <div class="col-lg-6">
+                                 <div class="form-group">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required>
+                                    @error('email')
+                                       <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                       </span>
+                                    @enderror
+                                 </div>
+                              </div>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                              <!-- Password -->
+                              <div class="col-lg-6">
+                                 <div class="form-group">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+                                    @error('password')
+                                       <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                       </span>
+                                    @enderror
+                                 </div>
+                              </div>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                              <!-- Confirm Password -->
+                              <div class="col-lg-6">
+                                 <div class="form-group">
+                                    <label for="password-confirm" class="form-label">Confirm Password</label>
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                 </div>
+                              </div>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                              <!-- Terms Checkbox -->
+                              <div class="d-flex justify-content-center">
+                                 <div class="form-check mb-3">
+                                    <input type="checkbox" class="form-check-input" id="terms" required>
+                                    <label class="form-check-label" for="terms">I agree with the terms of use</label>
+                                 </div>
+                              </div>
+                           </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                           <!-- Submit Button -->
+                           <div class="d-flex justify-content-center">
+                              <button type="submit" class="btn btn-primary">{{ __('Register') }}</button>
+                           </div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                                <a class="btn btn-link" href="{{ route('login') }}">
-                                    {{ __('Sudah punya akun Login?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                           <p class="mt-3 text-center">
+                              Already have an account? <a href="{{ route('login') }}" class="text-underline">Login here</a>.
+                           </p>
+                        </form>
+                     </div>
+                  </div> 
+               </div>
+            </div>    
+            <div class="sign-bg sign-bg-right">
+               <svg width="280" height="230" viewBox="0 0 431 398" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g opacity="0.05">
+                     <rect x="-157.085" y="193.773" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 -157.085 193.773)" fill="#3B8AFF"/>
+                     <rect x="7.46875" y="358.327" width="543" height="77.5714" rx="38.7857" transform="rotate(-45 7.46875 358.327)" fill="#3B8AFF"/>
+                     <rect x="61.9355" y="138.545" width="310.286" height="77.5714" rx="38.7857" transform="rotate(45 61.9355 138.545)" fill="#3B8AFF"/>
+                     <rect x="62.3154" y="-190.173" width="543" height="77.5714" rx="38.7857" transform="rotate(45 62.3154 -190.173)" fill="#3B8AFF"/>
+                  </g>
+               </svg>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+         </div>   
+      </div>
+   </section>
+</x-guest-layout>
