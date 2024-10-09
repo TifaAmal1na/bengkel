d1d76e0b2cf538ceb153d0027303d7ae86df708e
Faizal <91838778+FaizalLeviansyah@users.noreply.github.com>
2024-10-09 16:16:48 +0700

    Tambah Layout khusus Login dan css lalu menambahkan duplicate di layouts

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -42,64 +42,6 @@
     <!-- Page Wrapper -->
     <div id="wrapper">
 
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
-
-                <img src="/img/PALTransparan.png" style="width: 100%;">
-
-            </a>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('dashboard') }}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('notifikasi.index') }}">
-                    <i class="fas fa-star"></i>
-                    <span>Data Notifikasi</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('pekerjaan.index') }}">
-                    <i class="fas fa-fw fa-user-alt"></i>
-                    <span>Data Pekerjaan</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('personel.index') }}">
-                    <i class="fas fa-fw fa-user-alt"></i>
-                    <span>Data Personel</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('proyek.index') }}">
-                    <i class="fas fa-fw fa-file-archive"></i>
-                    <span>Data Proyek</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('revisi_gambar.index') }}">
-                    <i class="fas fa-images"></i>
-                    <span>Revisi Gambar</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('tools.index') }}">
-                    <i class="fas fa-tools"></i>
-                    <span>Tools</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('workload_analysis.index') }}">
-                    <i class="fas fa-industry"></i>
-                    <span>Workload Analysis</span></a>
-            </li>
-
-
         </ul>
         <!-- End of Sidebar -->
 
@@ -277,38 +219,7 @@
                             </div>
                         </li> --}}
 
-                        <div class="topbar-divider d-none d-sm-block"></div>
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal"
-                                    data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
+                        <div class="topbar-divider d-none d-sm-block"></div>    
 
                     </ul>
 
