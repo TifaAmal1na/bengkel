310e60d7c52ef1493015932506878003e5d330bc
Faizal <91838778+FaizalLeviansyah@users.noreply.github.com>
2024-10-15 09:50:20 +0700

    debug auth pada forgot password page

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\ResetPasswordCustom;
 
 class User extends Authenticatable
 {
@@ -43,5 +43,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordCustom($token));
     }
 }
