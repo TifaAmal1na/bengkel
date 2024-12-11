c01445ad33a677fe9c700a1c52a601c1e8648aa4
Ananda Galih Pratiwi <nandaglhp@gmail.com>
2024-12-11 13:35:20 +0700

    berhasil menghilangkan tanggal mulai-selesai

--- resources/views/pekerjaan/create.blade.php
+++ resources/views/pekerjaan/create.blade.php
@@ -1,74 +1,74 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col mt-2 mb-2">
-            <div class="card">
-                <div class="card-header py-3">
-                    <div class="m-0 font-weight-bold text-primary">Tambah Pekerjaan</div>
-                </div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('pekerjaan.store') }}">
-                        @csrf
-                        
-                        <div class="form-group">
-                            <label for="ID_PROYEK">Proyek:</label>
-                            <select name="ID_PROYEK" class="form-control" required>
-                                <option value="" disabled selected>Pilih Proyek</option>
-                                @foreach($proyekList as $proyek)
-                                    <option value="{{ $proyek->ID_PROYEK }}">{{ $proyek->NAMA }}</option>
-                                @endforeach
-                            </select>
-                        </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col mt-2 mb-2">
+                <div class="card">
+                    <div class="card-header py-3">
+                        <div class="m-0 font-weight-bold text-primary">Tambah Pekerjaan</div>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('pekerjaan.store') }}">
+                            @csrf
 
-                        <div class="form-group">
-                            <label for="ID_GRAFIK">Standard:</label>
-                            <select name="ID_GRAFIK" class="form-control" required>
-                                <option value="" disabled selected>Pilih Standard</option>
-                                @foreach($grafikList as $standard)
-                                    <option value="{{ $standard->ID_GRAFIK }}">{{ $standard->STANDARD }}</option>
-                                @endforeach
-                            </select>
-                        </div>
+                            <div class="form-group">
+                                <label for="ID_PROYEK">Proyek:</label>
+                                <select name="ID_PROYEK" class="form-control" required>
+                                    <option value="" disabled selected>Pilih Proyek</option>
+                                    @foreach ($proyekList as $proyek)
+                                        <option value="{{ $proyek->ID_PROYEK }}">{{ $proyek->NAMA }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="NAMA">Nama Pekerjaan:</label>
-                            <input type="text" name="NAMA" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="STATUS">Status:</label>
-                            <select name="STATUS" class="form-control" required>
-                                <option value="" disabled selected>Pilih Status</option>
-                                <option value="selesai">Selesai</option>
-                                <option value="dalam proses">Dalam Proses</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="KATEGORI">Kategori:</label>
-                            <input type="text" name="KATEGORI" class="form-control" required>
-                        </div>
+                            <div class="form-group">
+                                <label for="ID_GRAFIK">Standard:</label>
+                                <select name="ID_GRAFIK" class="form-control" required>
+                                    <option value="" disabled selected>Pilih Standard</option>
+                                    @foreach ($grafikList as $standard)
+                                        <option value="{{ $standard->ID_GRAFIK }}">{{ $standard->STANDARD }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="TANGGAL_MULAI">Tanggal Mulai:</label>
-                            <input type="date" name="tanggal_mulai" class="form-control" required>
-                        </div>
+                            <div class="form-group">
+                                <label for="NAMA">Nama Pekerjaan:</label>
+                                <input type="text" name="NAMA" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="STATUS">Status:</label>
+                                <select name="STATUS" class="form-control" required>
+                                    <option value="" disabled selected>Pilih Status</option>
+                                    <option value="selesai">Selesai</option>
+                                    <option value="dalam proses">Dalam Proses</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="KATEGORI">Kategori:</label>
+                                <input type="text" name="KATEGORI" class="form-control" required>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="TANGGAL_SELESAI">Tanggal Selesai:</label>
-                            <input type="date" name="tanggal_selesai" class="form-control" required>
-                        </div>
+                            <div class="form-group">
+                                <label for="TANGGAL_MULAI">Tanggal Mulai:</label>
+                                <input type="date" name="tanggal_mulai" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="TANGGAL_SELESAI">Tanggal Selesai:</label>
+                                <input type="date" name="tanggal_selesai" class="form-control" required>
+                            </div>
 
 
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                            {{-- <button class="btn btn-warning" onclick="window.history.back()">Kembali</button> --}}
-                            <a href="{{ route('pekerjaan.index') }}" class="btn btn-warning">Kembali</a>
-                        </div>
-                    </form>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                {{-- <button class="btn btn-warning" onclick="window.history.back()">Kembali</button> --}}
+                                <a href="{{ route('pekerjaan.index') }}" class="btn btn-warning">Kembali</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/pekerjaan/index.blade.php
+++ resources/views/pekerjaan/index.blade.php
@@ -1,72 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col mt-2 mb-2">
-            <div class="card">
-                <div class="card-header py-3">
-                    <div class="m-0 font-weight-bold text-primary">Daftar Pekerjaan</div>
-                    <div class="float-right">
-                        <a href="{{ route('pekerjaan.create') }}" class="btn btn-sm btn-primary shadow-sm">ADD</a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col mt-2 mb-2">
+                <div class="card">
+                    <div class="card-header py-3">
+                        <div class="m-0 font-weight-bold text-primary">Daftar Pekerjaan</div>
+                        <div class="float-right">
+                            <a href="{{ route('pekerjaan.create') }}" class="btn btn-sm btn-primary shadow-sm">ADD</a>
                         </div>
-                    @endif
+                    </div>
+                    <div class="card-body">
+                        @if ($message = Session::get('success'))
+                            <div class="alert alert-success">
+                                <p>{{ $message }}</p>
+                            </div>
+                        @endif
 
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nama Pekerjaan</th>
-                                    <th>Standard</th>
-                                    <th>Nama Proyek</th>
-                                    <th>Status</th>
-                                    <th>Kategori</th>
-                                    <th>Tanggal Mulai Standard</th>
-                                    <th>Tanggal Selesai Standard</th>
-                                    <th>Tanggal Mulai</th>
-                                    <th>Tanggal Selesai</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($pekerjaan as $p)
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
                                     <tr>
-                                        <td>{{ $p->ID_PEKERJAAN }}</td>
-                                        <td>{{ $p->NAMA }}</td>
-                                        <td>{{ $p->grafik->STANDARD ?? 'Tidak Ada Standard' }}</td>
-                                        <td>{{ $p->proyek->NAMA ?? 'Tidak Ada Proyek' }}</td>
-                                        <td>{{ $p->STATUS }}</td>
-                                        <td>{{ $p->KATEGORI }}</td>
-                                        <td>{{ $p->grafik->TANGGAL_MULAI ?? '-' }}</td>
-                                        <td>{{ $p->grafik->TANGGAL_SELESAI ?? '-' }}</td>
-                                        <td>{{ $p->TANGGAL_MULAI }}</td>
-                                        <td>{{ $p->TANGGAL_SELESAI }}</td>
-                                        <td>
-                                            <div class="d-flex">
-                                                <a class="btn btn-primary mr-1" href="{{ route('pekerjaan.edit', $p->ID_PEKERJAAN) }}">Edit</a>
-                                                <form action="{{ route('pekerjaan.destroy', $p->ID_PEKERJAAN) }}" method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                                </form>
-                                            </div>
-                                        </td>
+                                        <th>ID</th>
+                                        <th>Nama Pekerjaan</th>
+                                        <th>Standard</th>
+                                        <th>Nama Proyek</th>
+                                        <th>Status</th>
+                                        <th>Kategori</th>
+                                        <th>Tanggal Mulai Standard</th>
+                                        <th>Tanggal Selesai Standard</th>
+                                        <th>Aksi</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($pekerjaan as $p)
+                                        <tr>
+                                            <td>{{ $p->ID_PEKERJAAN }}</td>
+                                            <td>{{ $p->NAMA }}</td>
+                                            <td>{{ $p->grafik->STANDARD ?? 'Tidak Ada Standard' }}</td>
+                                            <td>{{ $p->proyek->NAMA ?? 'Tidak Ada Proyek' }}</td>
+                                            <td>{{ $p->STATUS }}</td>
+                                            <td>{{ $p->KATEGORI }}</td>
+                                            <td>{{ $p->grafik->TANGGAL_MULAI ?? '-' }}</td>
+                                            <td>{{ $p->grafik->TANGGAL_SELESAI ?? '-' }}</td>
+                                            <td>
+                                                <div class="d-flex">
+                                                    <a class="btn btn-primary mr-1"
+                                                        href="{{ route('pekerjaan.edit', $p->ID_PEKERJAAN) }}">Edit</a>
+                                                    <form action="{{ route('pekerjaan.destroy', $p->ID_PEKERJAAN) }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
